98181ec62046e009cf3b8a251f8d937e73477c15
Matheus Ikenaga <51059596+MatheusIkenaga@users.noreply.github.com>
2020-05-11 21:59:12 -0300

    Testing python

--- Python/hello.py
+++ Python/hello.py
@@ -1,1 +1,20 @@
+#-*- coding: UTF-8 -*-
+"""
+No VS CODE os acentos não funcionaram
+"""
+#incluindo acentuação
+
 print ("Hello World")
+print ("Outra mensagem")
+
+#This is a comment
+""" 
+Multiple 
+Lines Comments
+"""
+
+print("test")
+
+
+
+print("Olá mundo")
